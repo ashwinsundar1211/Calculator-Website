ff194c3e381b76ab4e5ba3d76e4c8a05293ca2cb
ashwinsundar1211 <138789514+ashwinsundar1211@users.noreply.github.com>
2023-10-06 12:11:45 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function Add()
+{
+    var Add = 
+    Number(document.getElementById("1Num").value)+
+    Number(document.getElementById("2Num").value)
+    console.log(Add)
+}
+function Subtract()
+{
+    var Subtract = 
+    Number(document.getElementById("1Num").value)-
+    Number(document.getElementById("2Num").value)
+    console.log(Subtract)
+}
+function Multiply()
+{
+    var Multiply = 
+    Number(document.getElementById("1Num").value)*
+    Number(document.getElementById("2Num").value)
+    console.log(Multiply)
+}
+function Divide()
+{
+    var Divide = 
+    Number(document.getElementById("1Num").value)/
+    Number(document.getElementById("2Num").value)
+    console.log(Divide)
+}
